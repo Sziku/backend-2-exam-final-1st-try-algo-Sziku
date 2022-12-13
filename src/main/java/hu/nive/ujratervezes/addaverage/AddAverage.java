1a04c1e9376875061e5ac8518b43a98879451c5d
Sziku <105989158+Sziku@users.noreply.github.com>
2022-12-13 11:06:02 +0100

    add methods

--- src/main/java/hu/nive/ujratervezes/addaverage/AddAverage.java
+++ src/main/java/hu/nive/ujratervezes/addaverage/AddAverage.java
@@ -3,6 +3,20 @@ package hu.nive.ujratervezes.addaverage;
 public class AddAverage {
 
     public int[] addAverage(int[] numbers, int n) {
+        int average = 0;
+        for (int number : numbers){
+            average += number;
+        }
+
+        average = average / numbers.length;
+
+        for(int i = n - 1; i< numbers.length;){
+            int tmp = numbers[i];
+            numbers[i] = tmp+average;
+            i = i + n;
+        }
+
+        return numbers;
     }
 
     public static void main(String[] args) {
